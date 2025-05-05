9ea7e998ac323fc130486e4f1d7af5fab2c5abf9
Eduardo Lemus <eduardolemus3098@gmail.com>
2025-05-04 20:39:38 -0600

    logs en web

--- app/login.tsx
+++ app/login.tsx
@@ -1,27 +1,30 @@
 // app/login.tsx
-import React, { useState } from 'react';
-import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from "react";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 import { login, register } from "../services/authService";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
-      await login(email, password);
-      Alert.alert('¡Inicio de sesión exitoso!');
+      const singinResponse = await login(email, password);
+      console.log("Inicio de sesión exitoso:", singinResponse);
+      Alert.alert("¡Inicio de sesión exitoso!");
     } catch (error: any) {
-      Alert.alert('Error', error.message);
+      console.log("Error de inicio de sesión:", error);
+      Alert.alert("Error", error.message);
     }
   };
 
   const handleRegister = async () => {
+    console.log("Registrarse");
     try {
       await register(email, password);
-      Alert.alert('¡Usuario registrado exitosamente!');
+      Alert.alert("¡Usuario registrado exitosamente!");
     } catch (error: any) {
-      Alert.alert('Error', error.message);
+      Alert.alert("Error", error.message);
     }
   };
 
@@ -53,19 +56,19 @@ const styles = StyleSheet.create({
   container: {
     padding: 24,
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#fff'
+    justifyContent: "center",
+    backgroundColor: "#fff",
   },
   input: {
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     padding: 12,
     marginVertical: 8,
-    borderRadius: 6
+    borderRadius: 6,
   },
   title: {
     fontSize: 24,
-    textAlign: 'center',
-    marginBottom: 20
-  }
+    textAlign: "center",
+    marginBottom: 20,
+  },
 });
